5ae27074c7fcd8132697b2302353acc3bba8b964
alperkan1 <seanhack@gmail.com>
2021-09-12 12:02:06 +0000

    commit the values are correct

--- run.py
+++ run.py
@@ -18,5 +18,19 @@ def get_sales_data():
     print("Data should have 6 numbers , seperated with commas.")
     print("example: 10, 12, 30, 40, 50, 60\n")
     data_str = input("Enter your data here: ")
-    print(f"The data provided is{data_str}")
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+def validate_data(values):
+    """
+    inside the try converts all strins values into intergers .
+    Raise the Value error if the strings cannot be converted into a int or if there isnt 6 values.
+    """
+    try: 
+        if len(values) != 6:
+            raise ValueError(
+                f"expected 6 values required , you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"invalid data: {e}, please try again/n")
+
 get_sales_data() 
